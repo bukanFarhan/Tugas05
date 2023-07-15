44c5a78304092c4ea64b7b80d8f135a255fcb751
muhammad farhan <muhammad.2842@students.amikom.ac.id>
2023-07-15 13:42:56 +0700

    master

---
+++ 19.11.2842_Tugas05/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _19._11._2842_Tugas05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Numbers numbers = new Numbers();
+           numbers.CariNilaiMin(20, 21);
+           numbers.CariNilaiMin(100, 20, 11);
+           numbers.CariNilaiMax(20, 21);
+           numbers.CariNilaiMax(100, 20, 11);
+        }
+    }
+}
